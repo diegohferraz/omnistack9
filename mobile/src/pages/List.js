4ff23383067c85c5947754a971d6aa76a50d64e7
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-10-05 20:13:36 -0300

    react native routing setup

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function List() {
+    return (
+      <View>
+        <Text>Hello List</Text>
+      </View>
+    );
+  }
