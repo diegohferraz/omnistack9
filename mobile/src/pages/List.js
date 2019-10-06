6fa410ee2eb075b28f40606808eb3c618f205593
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-10-05 22:08:08 -0300

    adds spot listing

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+import { StyleSheet, Image, AsyncStorage, Text, ScrollView ,SafeAreaView } from 'react-native';
+
+import logo from '../../assets/logo.png'
+import SpotList from '../../components/SpotList';
 
 export default function List() {
+    const [email, setEmail] = useState('')
+    const [techs, setTechs] = useState([])
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs
+            .split(',')
+            .map(tech => tech.trim())
+
+            setTechs(techsArray)
+        })
+        
+    }, [])
+
+
     return (
-      <View>
-        <Text>Hello List</Text>
-      </View>
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} /> )}
+            </ScrollView>
+        </SafeAreaView>
     );
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+
+    logo:{
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 30
+    }
+})
