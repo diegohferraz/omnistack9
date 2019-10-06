3964e2f960f82b01db26e58a617677cec3de41d7
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-10-06 09:45:31 -0300

    creates booking reservation flow on mobile app

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,66 +1,71 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, Alert, TouchableOpacity, AsyncStorage, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+    SafeAreaView,
+    Alert,
+    AsyncStorage,
+    StyleSheet,
+    TextInput,
+    Text,
+    TouchableOpacity
+} from 'react-native';
+
 import api from '../../services/api';
 
 export default function Book({ navigation }) {
-    const [date, setDate] = useState('')
-    const id = navigation.getParam('id')
+    const [date, setDate] = useState('');
+    const id = navigation.getParam('id');
 
-    async function handleSubmit () {
-        const user_id = AsyncStorage.getItem('user')
+    async function handleSubmit() {
+        const user_id = await AsyncStorage.getItem('user');
 
-        await api.post(`/spots/${id}/bookings`, {
+        const response = await api.post(`/spots/${id}/bookings`, {
             date
         }, {
             headers: { user_id }
-        })
+        });
 
-        Alert.alert('Solicaitacao de reserva enviada!')
+        Alert.alert('Solicitação de reserva enviada.');
 
-        navigation.navigate('List')
-    }
+        navigation.navigate('List');
+    };
 
-    function handleCancel () {
-        navigation.navigate('List')
-    }
+    function handleCancel() {
+        navigation.navigate('List');
+    };
 
     return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.label}>DATA DE INTERESSE *</Text>
-        <TextInput
-            style={styles.input}
-            placeholder='Qual data voce quer reservar ?'
-            placeholderTextColor='#999'
-            autoCapitalize='words'
-            autoCorrect={false}
-            value={date}
-            onChangeText={setDate}
-        />
-        <TouchableOpacity
-            onPress={handleSubmit}
-            style={styles.button}>
-            <Text style={styles.buttonText}>Solicitar reserva</Text>
-        </TouchableOpacity>
+        <SafeAreaView style={style.container}>
+            <Text style={style.label}>DATA DE INTERESSE *</Text>
+            <TextInput
+                style={style.input}
+                placeholder="Qual data você quer reservar?"
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect={false}
+                value={date}
+                onChangeText={setDate}
+            />
 
-        <TouchableOpacity
-            onPress={handleCancel}
-            style={styles.cancelButton}>
-            <Text style={styles.buttonText}>Cancelar</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
+            <TouchableOpacity onPress={handleSubmit} style={style.button}>
+                <Text style={style.buttonText}>Solicitar reserva</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleCancel} style={[style.button, style.cancelButton]}>
+                <Text style={style.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
-  }
+}
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
     container: {
-        margin: 30
+        margin: 30,
+        marginTop: 60,
     },
 
     label: {
         fontWeight: 'bold',
         color: '#444',
         marginBottom: 8,
-        marginTop: 30
     },
 
     input: {
@@ -79,20 +84,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#f05a5b',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 2
-      },
+        borderRadius: 2,
+    },
 
-      cancelButton: {
-        height: 42,
+    cancelButton: {
         backgroundColor: '#ccc',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 2
-      },
+        marginTop: 10,
+    },
 
-      buttonText: {
-          color: '#fff',
-          fontWeight: 'bold',
-          fontSize: 16
-      }
+    buttonText: {
+        color: "#FFF",
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
 })
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,45 +1,61 @@
 import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
+import {
+  Alert,
+  SafeAreaView,
+  ScrollView,
+  AsyncStorage,
+  Image,
+  StyleSheet
+} from 'react-native';
 
-import { StyleSheet, Image, AsyncStorage, Text, ScrollView ,SafeAreaView } from 'react-native';
+import logo from '../../assets/logo.png';
 
-import logo from '../../assets/logo.png'
 import SpotList from '../../components/SpotList';
 
+
 export default function List() {
-    const [email, setEmail] = useState('')
-    const [techs, setTechs] = useState([])
+  const [techs, setTechs] = useState([]);
 
-    useEffect(() => {
-        AsyncStorage.getItem('techs').then(storagedTechs => {
-            const techsArray = storagedTechs
-            .split(',')
-            .map(tech => tech.trim())
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.0.9:3333', {
+        query: { user_id }
+      })
 
-            setTechs(techsArray)
-        })
-        
-    }, [])
+      socket.on('booking_response', booking => {
+        console.log("socket");
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? "APROVADA" : "REJEITADA"}`);
+      })
+    })
+  }, []);
 
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+      setTechs(techsArray);
+    })
+  }, []);
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Image style={styles.logo} source={logo} />
-            <ScrollView>
-                {techs.map(tech => <SpotList key={tech} tech={tech} /> )}
-            </ScrollView>
-        </SafeAreaView>
-    );
-  }
+  return (
+    <SafeAreaView style={style.container}>
+      <Image style={style.logo} source={logo} />
+      <ScrollView>
+        {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1
-    },
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
 
-    logo:{
-        height: 32,
-        resizeMode: 'contain',
-        alignSelf: 'center',
-        marginTop: 30
-    }
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 50,
+  },
 })
