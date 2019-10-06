3c71111952bf6514b051ed983ccc7808d9cca7a5
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-10-06 09:27:51 -0300

    creates booking reservation approval and rejection flow

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,23 +11,29 @@ const app = express();
 const server = http.Server(app)
 const io = socketio(server)
 
-io.on('connection', socket => {
-  console.log('conectado', socket.id)
-})
-
 mongoose.connect('mongodb+srv://sandbox:sandbox@diegolabs-y2jf2.mongodb.net/omnistack?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
-app.use(routes)
-
-app.get('/', (req, res) => { 
-  return res.json({message: 'hello world'});
-});
+app.use(routes);
 
 server.listen(3333);
 
