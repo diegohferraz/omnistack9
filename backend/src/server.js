33f15c6c99939f4a2a546d61244d8440b63ba4dd
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-10-06 08:44:12 -0300

    add socket configuration to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http')
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app)
+const io = socketio(server)
+
+io.on('connection', socket => {
+  console.log('conectado', socket.id)
+})
 
 mongoose.connect('mongodb+srv://sandbox:sandbox@diegolabs-y2jf2.mongodb.net/omnistack?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -21,5 +29,5 @@ app.get('/', (req, res) => {
   return res.json({message: 'hello world'});
 });
 
-app.listen(3333);
+server.listen(3333);
 
